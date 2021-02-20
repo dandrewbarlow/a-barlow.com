a43690df007bea81c4da76850216f00e64555190
Andrew Barlow <dandrewbarlow@gmail.com>
2021-02-19 22:27:13 -0700

    added script for nav and remove DS_Store's

---
+++ js/nav.js
@@ -0,0 +1,11 @@
+navContent = `
+        <ul>
+			<li><a href="index.html">Home</a></li>
+			<li><a href="about-me.html">About Me</a></li>
+			<li><a href="art.html">Art</a></li>
+			<li><a href="projects.html">My Projects</a></li>
+        </ul>
+        `;
+
+nav = document.getElementById('nav');
+nav.innerHTML = navContent;
