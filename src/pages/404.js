f823fc4db399e4be5a928576515f339e3036f900
Andrew <dandrewbarlow@gmail.com>
2021-10-02 14:44:56 -0600

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,21 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React, { Component } from 'react'
+import Layout from '../components/layout'
+import fourOhFour from '../images/404/404.jpg'
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+export default class NotFoundPage extends Component {
+  render() {
+    return (
+      <Layout title="404">
+        <h2>404: Page not found</h2>
+        <div className="linkGallery">
+          <figure>
+            <img src={fourOhFour} alt="The page does not exist" />
+            <figcaption>
+              Sorry, but it looks like this is not a page.
+            </figcaption>
+          </figure>
+        </div>
+      </Layout>
+    )
+  }
 }
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
-const NotFoundPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
-
-export default NotFoundPage
